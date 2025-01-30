04234f39111fa362cc1172473bcaf261a3752bd1
kamisup1 <158269001+kamisup1@users.noreply.github.com>
2025-01-30 18:59:51 +0100

    Add files via upload

---
+++ lab5/File1.fs
@@ -0,0 +1,52 @@
+﻿module File1
+
+
+// 1 ciag fibonacciego
+let rec fibonacci n =
+    if n <= 1 then n
+    else fibonacci (n - 1) + fibonacci (n - 2)
+
+let fibonacciTail n =
+    let rec aux n acc1 acc2 =
+        if n = 0 then acc1
+        elif n = 1 then acc2
+        else aux (n - 1) acc2 (acc1 + acc2)
+    aux n 0 1
+
+// 2 drzewo binarne
+type Tree<'T> =
+    | Empty
+    | Node of 'T * Tree<'T> * Tree<'T>
+
+let rec searchElement tree value =
+    match tree with
+    | Empty -> false
+    | Node (x, left, right) when x = value -> true
+    | Node (_, left, right) -> 
+        searchElement left value || searchElement right value
+
+// 3 permutacja
+let rec permute lst =
+    match lst with
+    | [] -> [[]]
+    | head :: tail ->
+        let perms = permute tail
+        List.collect (fun perm ->
+            [for i in 0 .. List.length perm do
+                yield List.insertAt i head perm]) perms
+
+// zad4 wieza hanoi
+let rec hanoi n from_ to_ aux =
+    if n > 0 then
+        hanoi (n - 1) from_ aux to_
+        printfn "Przenieś dysk %d z %s na %s" n from_ to_
+        hanoi (n - 1) aux to_ from_
+
+// 5quick sort
+let rec quicksort lst =
+    match lst with
+    | [] -> []
+    | pivot :: tail ->
+        let lessThanPivot = List.filter (fun x -> x < pivot) tail
+        let greaterThanPivot = List.filter (fun x -> x > pivot) tail
+        (quicksort lessThanPivot) @ [pivot] @ (quicksort greaterThanPivot)
---
+++ lab5/Program.fs
@@ -0,0 +1,34 @@
+﻿
+open File1
+
+[<EntryPoint>]
+let main argv =
+
+
+// zad1
+    let n = 10
+    printfn "Ciąg Fibonacciego dla n = %d (rekurencja): %d" n (fibonacci n)
+    printfn "Ciąg Fibonacciego dla n = %d (rekurencja ogonowa): %d" n (fibonacciTail n)
+
+// Zad2
+    let tree = Node(10, Node(5, Empty, Empty), Node(15, Empty, Empty))
+    let searchResult = searchElement tree 5
+    printfn "Czy element 5 istnieje w drzewie? %b" searchResult
+
+// zad3
+    let numbers = [1; 2; 3]
+    let permutations = permute numbers
+    printfn "Permutacje listy [1; 2; 3]:"
+    permutations |> List.iter (fun perm -> printfn "%A" perm)
+
+ // zad4
+    printfn "Rozwiązywanie problemu Wież Hanoi dla 3 dysków:"
+    hanoi 3 "A" "C" "B"
+
+//zad5
+    let unsortedList = [5; 3; 8; 1; 2; 7; 6]
+    let sortedList = quicksort unsortedList
+    printfn "Posortowana lista (QuickSort): %A" sortedList
+
+
+    0 
