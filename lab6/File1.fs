ddd77545d98975cec5173e25fbcdfc94f3c073af
kamisup1 <158269001+kamisup1@users.noreply.github.com>
2025-01-30 20:30:33 +0100

    Add files via upload

---
+++ lab6/File1.fs
@@ -0,0 +1,36 @@
+﻿module File1
+
+open System
+
+//zad1
+let countWordsAndChars (text: string) =
+    let words = text.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries)
+    let charCount = text.Replace(" ", "").Length
+    (words.Length, charCount)
+
+//zad2
+let isPalindrome (text: string) =
+    let cleanedText = text.Replace(" ", "").ToLower()
+    cleanedText = String(Array.rev (cleanedText.ToCharArray()))
+
+//zad3
+let removeDuplicates (words: string list) =
+    words |> List.distinct
+
+//zad4
+let changeFormat (input: string) =
+    let parts = input.Split(';') |> Array.map (fun x -> x.Trim())
+    let firstName = parts.[0]
+    let lastName = parts.[1]
+    let age = parts.[2]
+    sprintf "%s, %s (%s lat)" lastName firstName age
+
+//zad5
+let findLongestWord (text: string) =
+    let words = text.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries)
+    let longestWord = words |> Array.maxBy String.length
+    (longestWord, longestWord.Length)
+
+//zad6
+let replaceWord (text: string) (oldWord: string) (newWord: string) =
+    text.Replace(oldWord, newWord)
---
+++ lab6/Program.fs
@@ -0,0 +1,54 @@
+﻿open System
+open File1
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "Zadanie 1: Liczba słów i znaków"
+    printfn "Podaj tekst:"
+    let inputText = Console.ReadLine()
+    let (wordCount, charCount) = countWordsAndChars inputText
+    printfn "Liczba słów: %d" wordCount
+    printfn "Liczba znaków (bez spacji): %d" charCount
+
+
+    printfn "\nZadanie 2: Sprawdzenie palindromu"
+    printfn "Podaj tekst do sprawdzenia:"
+    let inputText2 = Console.ReadLine()
+    if isPalindrome inputText2 then
+        printfn "Tekst jest palindromem."
+    else
+        printfn "Tekst nie jest palindromem."
+
+
+    printfn "\nZadanie 3: Usuwanie duplikatów"
+    printfn "Podaj słowa oddzielone spacjami:"
+    let inputText3 = Console.ReadLine()
+    let words = inputText3.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries) |> List.ofArray
+    let uniqueWords = removeDuplicates words
+    printfn "Unikalne słowa: %A" uniqueWords
+
+    printfn "\nZadanie 4: Zmiana formatu tekstu"
+    printfn "Podaj dane w formacie 'imię; nazwisko; wiek':"
+    let inputText4 = Console.ReadLine()
+    let formattedText = changeFormat inputText4
+    printfn "Zmodyfikowany format: %s" formattedText
+
+
+    printfn "\nZadanie 5: Znajdowanie najdłuższego słowa"
+    printfn "Podaj tekst:"
+    let inputText5 = Console.ReadLine()
+    let (longestWord, length) = findLongestWord inputText5
+    printfn "Najdłuższe słowo to: '%s' o długości: %d" longestWord length
+
+    printfn "\nZadanie 6: Wyszukiwanie i zamiana"
+    printfn "Podaj tekst:"
+    let inputText6 = Console.ReadLine()
+    printfn "Podaj słowo do wyszukania:"
+    let oldWord = Console.ReadLine()
+    printfn "Podaj słowo do zamiany:"
+    let newWord = Console.ReadLine()
+    let modifiedText = replaceWord inputText6 oldWord newWord
+    printfn "Zmodyfikowany tekst: %s" modifiedText
+
+    0 
